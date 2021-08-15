c2f8468caa70368fd8ec8d75c3ed40af426e785c
Syakil Ahmad Ginanjar <46291036+syakil@users.noreply.github.com>
2021-08-15 16:23:59 +0700

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,10 +23,12 @@ class HomeController extends Controller
      */
     public function index(){
 
-        if(Auth::user()->level == 1) return view('home.admin', compact('param_tgl'));
-          elseif(Auth::user()->level == 3) return view('home.accounting', compact('param_tgl'));
-          elseif(Auth::user()->level == 2) return view('home.helpdesk', compact('param_tgl'));
-          elseif(Auth::user()->level == 4) return view('home.spvs_helpdesk', compact('param_tgl'));
-          else return view('/login', compact('setting'));
+
+        return view('home');
+        // if(Auth::user()->level == 1) return view('home.admin', compact('param_tgl'));
+        //   elseif(Auth::user()->level == 3) return view('home.accounting', compact('param_tgl'));
+        //   elseif(Auth::user()->level == 2) return view('home.helpdesk', compact('param_tgl'));
+        //   elseif(Auth::user()->level == 4) return view('home.spvs_helpdesk', compact('param_tgl'));
+        //   else return view('/login', compact('setting'));
     }
 }
