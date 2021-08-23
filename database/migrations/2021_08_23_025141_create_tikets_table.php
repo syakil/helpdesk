485d23ea4f912a731b79187fb01ce15dbc3f606b
syakil <syakilahmadginanjar@gmail.com>
2021-08-23 15:50:43 +0700

    tiket model database

---
+++ database/migrations/2021_08_23_025141_create_tikets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTiketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tiket', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('no_tiket');
+            $table->integer('user_id');
+            $table->string('cif')->nullable();
+            $table->date('tanggal_transaksi');
+            $table->string('kode_transaksi');
+            $table->string('jenis_transaksi');
+            $table->text('keterangan');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tikets');
+    }
+}
